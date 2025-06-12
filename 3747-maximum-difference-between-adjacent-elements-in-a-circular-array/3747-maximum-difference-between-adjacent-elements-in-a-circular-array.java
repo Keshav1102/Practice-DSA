75e178198c856bcaae0c3876154d03e5f8af973a
Tushank <tushankkumar.44112@gmail.com>
2025-06-12 13:08:56 +0530

    Time: 1 ms (98.36%), Space: 43.1 MB (95.41%) - LeetHub

--- 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/3747-maximum-difference-between-adjacent-elements-in-a-circular-array.java
+++ 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/3747-maximum-difference-between-adjacent-elements-in-a-circular-array.java
@@ -1,17 +1,29 @@
 class Solution {
     public int maxAdjacentDistance(int[] nums) {
         int len = nums.length;
-        int[] arr = new int[len];
+        // int[] arr = new int[len];
         int max=Integer.MIN_VALUE;
-        for(int i=0;i<len-1;i++){
-            arr[i] = Math.abs(nums[i]-nums[i+1]);
-            if(arr[i]>max){
-                max = arr[i];
-            }
+        // for(int i=0;i<len-1;i++){
+        //     arr[i] = Math.abs(nums[i]-nums[i+1]);
+        //     if(arr[i]>max){
+        //         max = arr[i];
+        //     }
+        // }
+        // arr[len-1] = Math.abs(nums[len-1]-nums[0]);
+        // if(arr[len-1]>max){
+        //     max = arr[len-1];
+        // }
+        // return max;
+
+        int num = Math.abs(nums[0]-nums[len-1]);
+        if(num>max){
+            max = num;
         }
-        arr[len-1] = Math.abs(nums[len-1]-nums[0]);
-        if(arr[len-1]>max){
-            max = arr[len-1];
+        for(int i=0;i<len-1;i++){
+            nums[i] = Math.abs(nums[i]-nums[i+1]);
+            if(max<nums[i]){
+                max = nums[i];
+            }
         }
         return max;
     }
