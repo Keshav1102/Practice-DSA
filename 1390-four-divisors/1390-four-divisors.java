def453495f73637623fc13b87235d5bcd8e58a91
Tushank <tushankkumar.44112@gmail.com>
2026-01-05 00:24:19 +0530

    Time: 19 ms (54.04%), Space: 47.1 MB (14.65%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int sum = 0;
+        HashMap<Integer, Integer> cache = new HashMap<>();
+
+        for (int num : nums) {
+            int divisorSum = cache.getOrDefault(num, -1);
+            if (divisorSum == -1) {
+                divisorSum = getFourDivisorSum(num);
+                cache.put(num, divisorSum);
+            }
+            sum += divisorSum;
+        }
+        return sum;
+    }
+
+    private int getFourDivisorSum(int num) {
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                int d1 = i;
+                int d2 = num / i;
+
+                if (d1 == d2) {
+                    count++;
+                    sum += d1;
+                } else {
+                    count += 2;
+                    sum += d1 + d2;
+                }
+
+                if (count > 4) {
+                    return 0;
+                }
+            }
+        }
+        return count == 4 ? sum : 0;
+    }
+}
